e6678bc76a48f021f0703aff8d686f74f3e4201d
siddhantpandey <siddhantpandey59@gmail.com>
2018-04-15 23:28:54 +0530

    11:28 PM 15-04-2018

--- donation.php
+++ donation.php
@@ -22,6 +22,29 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <style>
+      #storyboard1 {
+    border-radius: 25px;
+    background: #f4f442;
+    padding: 20px; 
+    
+    display: inline-block;
+    }
+    #storyboard2 {
+    border-radius: 25px;
+    background: #ff99ff;
+    padding: 20px; 
+    
+    display: inline-block;
+    }
+        #storyboard {
+    border-radius: 25px;
+    border: 2px solid #73AD21;
+    padding: 20px; 
+    display: inline-block;
+    }
+
+    </style>
   </head>
   <body>	
 	<header id="header">
@@ -60,28 +83,32 @@
 			<div class="row">
 				<figure class="effect-chico">
 						<div class="col-md-3 wow fadeInUp" data-wow-offset="0" data-wow-delay="0.3s">
-							<a href="begger.php" class="flipLightBox">
+						
+							<a href="begger.php" id="begger" class="flipLightBox" onclick="document.location=this.id+'.php';return false;">
 							<img src="img/donation/p1.jpg" class="img-responsive" alt="" height="1600px" width="1400px" title="Beggar Kids">
 							
 							</a>
 						</div>
 					</figure>
+					&ensp;
 				<figure class="effect-chico">
 						<div class="col-md-3 wow fadeInUp" data-wow-offset="0" data-wow-delay="0.3s">
-							<a href="street.php" class="flipLightBox"><img src="img/donation/p2.jpg" class="img-responsive" alt="" title="Homeless"></a>
+							<a href="street.php" id="street" class="flipLightBox" onclick="document.location=this.id+'.php';return false;"><img src="img/donation/p2.jpg" class="img-responsive" alt="" title="Homeless"></a>
 						</div>
 					</figure>	
+					&ensp;
 				<figure class="effect-chico">
 						<div class="col-md-3 wow fadeInDown" data-wow-offset="0" data-wow-delay="0.3s">
-							<a href="skl.php" class="flipLightBox">
+							<a href="skl.php" class="flipLightBox" id="skl" onclick="document.location=this.id+'.php';return false;">
 							<img src="img/donation/p3.jpg" class="img-responsive" alt="" title="Schooling">
 							
 							</a>
 						</div>
 					</figure>
+					&ensp;
 				<figure class="effect-chico">
 						<div class="col-md-3 wow fadeInDown" data-wow-offset="0" data-wow-delay="0.3s">
-							<a href="childlab.php" class="flipLightBox">
+							<a href="childlab.php" class="flipLightBox" id="childlab" onclick="document.location=this.id+'.php';return false;">
 							<img src="img/donation/p4.jpg" class="img-responsive" alt="" title="Child Labour">
 							
 							</a>
@@ -91,8 +118,35 @@
 		</div>
 		
 	</div><!--/#gallery-->
-
-
+	<div id="storyboard">
+	<div id="storyboard1">
+	    <p>INDIANS waste as much food as the whole of United Kingdom consumes – a statistic that may not so much indicative of our love of surfeit, as it is of our population. Still, food wastage is an alarming issue in India. Our street and garbage bins, landfills have sufficient proof to prove it.
+Weddings, canteens, hotels, social and family functions, households spew out so much food.  According to the United Nations Development Programme, up to 40% of the food produced in India is wasted. About 21 million tonnes of wheat are wasted in India and 50% of all food across the world meets the same fate and never reaches the needy. In fact, according to the agriculture ministry, Rs. 50,000 crore worth of food produced is wasted every year in the country.</p>    
+	</div>
+      <br><br>
+  <div id="storyboard1">
+     <p>25% of fresh water used to produce food is ultimately wasted, even as millions of people still don’t have access to drinking water. When you calculate the figures in cubic kilometers, this is a bit more than an average river. Even though the world produces enough food to feed twice the world’s present population, food wastage is ironically behind the billions of people who are malnourished. The number of hungry people in India has increased by 65 million more than the population of France. According to a survey by Bhook (an organization working towards reducing hunger) in 2013, 20 crore Indians sleep hungry on any given night. About 7 million children died in 2012 because of hunger/malnutrition. Acres of land are deforested to grow food. Approximately 45% of India’s land is degraded primarily due to deforestation, unsustainable agricultural practices, and excessive groundwater extraction to meet the food demand.
+300 million barrels of oil are used to produce food that is ultimately wasted.</p>
+      </div>
+      
+      
+      </div>
+      <br/><br/>
+      <div id="storyboard">
+	<div id="storyboard2">
+	    <p>Child labour is prominent in rural India - 80% of working children live in India's villages, where most of them work in agriculture. Some of them also work in household industries and are employed in home-based businesses. Children between 14-17 years engaged in hazardous work account for 62.8% of the India's child labour workforce, 10% of whom are hired in family enterprises. Over half of working adolescents do not study. This number is higher for adolescents doing dangerous work. It is not surprising that more boys than girls (38.7 million vs. 8.8 million) are forced into doing hazardous work (according to International Labour Organization’s World Report on Child Labour 2015).</p>    
+	</div>
+      <br><br>
+  <div id="storyboard2">
+     <p>One in every 11 children in India is working. Child labour has been decreasing at an abysmal rate of 2.2% per year from 2001 to 2011, as per an analysis of census data by non-governmental organization CRY (Child Rights and You). 80% of working children are based in rural areas and three out of four of these children work in agriculture, as cultivators or in household industries, most of which are home-based employments. More than half of the 5.5 million working children in India are concentrated in five states—Bihar, Uttar Pradesh, Rajasthan, Madhya Pradesh and Maharashtra. Adolescents doing hazardous work form 20.7% of those employed in this age group, while almost 25% of adults work in hazardous conditions. Adolescents between 15 and 17 years of age doing hazardous work form 62.8% of the overall child labour population.</p>
+      </div>
+      <br/><br/>
+      <div id="storyboard2">
+     <p>Nearly 10% of adolescents working in hazardous conditions are working in family enterprises. 56% of the working adolescents are no longer studying. And 70% of those in hazardous conditions are not studying. More boys (38.7 million) than girls (8.8 million) are involved in hazardous work. While the incidence of hazardous work among adolescents is highest in Nicaragua, the number of adolescents in hazardous work is greatest in India (2.4 million).
+         <b>Source:</b> <i>International Labour Organization’s World Report on Child Labour 2015 and CRY recent analysis of the Census 2011</i></p>
+      </div>
+      
+      </div>
    
    <footer>
 						
